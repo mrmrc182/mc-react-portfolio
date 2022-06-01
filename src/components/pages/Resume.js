bec345589076313479553040b213b52e116bfe23
Matthew Carlson <carlson.matt.r@gmail.com>
2022-06-01 15:10:57 -0700

    added resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,9 +1,13 @@
 import React from "react";
+import mcResume from "../../images/MC_Resume.png"
 
-export default function Resume () {
-    return (
-        <div>
-            <h3 className="name">You can view Matt's resume here</h3>
+export default function Resume() {
+  return (
+    <div className="resume-page">
+      <h3 className="name">You can view Matt's resume here</h3>
+      <div>
+        <img className="resume" src={mcResume} alt="Matt's Resume"></img>
         </div>
-    )
+    </div>
+  );
 }
