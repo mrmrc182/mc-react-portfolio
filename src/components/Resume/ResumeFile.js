51bc924436d1709c02f4c9163c183db154c328d4
Matthew Carlson <carlson.matt.r@gmail.com>
2022-06-01 16:51:37 -0700

    asking BCS for styling assistance

---
+++ src/components/Resume/ResumeFile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import mcResume from "../../images/MC_Resume.png";
+
+export default function File() {
+return(
+<div>
+  <img className="resume" src={mcResume} alt="Matt's Resume"></img>
+</div>
+)
+}
+
+
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,13 +1,11 @@
 import React from "react";
-import mcResume from "../../images/MC_Resume.png"
+import File from "../Resume/ResumeFile"
 
 export default function Resume() {
   return (
     <div className="resume-page">
       <h3 className="name">You can view Matt's resume here</h3>
-      <div>
-        <img className="resume" src={mcResume} alt="Matt's Resume"></img>
-        </div>
+        <File />
     </div>
   );
 }
