51e62b36c64733d63075a7192d8033a2c2170bda
Matthew Carlson <carlson.matt.r@gmail.com>
2022-05-31 20:18:40 -0700

    projects now have a hover effect

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,19 +1,16 @@
 import React, { useState } from 'react';
 import NavBar from './Navigation/NavBar';
-import Home from './pages/Home';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Resume from './pages/Resume';
 import Contact from './pages/Contact';
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('Home');
+  const [currentPage, setCurrentPage] = useState('About');
 
   // This function is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
     switch (currentPage) {
-      case 'Home':
-        return <Home />;
       case 'About':
         return <About />;
       case 'Portfolio':
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import Headshot from "../../images/headshot.jpg";
 
 export default function About() {
   return (
     <div>
-      <h1 class="font-link">About Matt</h1>
-      <p class="paragraph-font">
-        Matt is a San Diego native born and raised.  Graduate of the UC Irvine Class of 2018, and is now learning React JS at the UCSD's Full Stack Web Development Bootcamp.
+      <h4 className="name">Last of the lost, first of the fashionably late</h4>
+      <img className = "picture" alt="A headshot of Mr. Carlson" src={Headshot}></img>
+      <h1 class="name">About Matt</h1>
+      <p class="name">
+        Matt is a San Diego native born and raised.  <br></br>Graduate of the UC Irvine Class of 2018 with a BA in Political Science, he is now learning software engineering at UCSD's Full Stack Web Development Bootcamp.
 
         <br></br>
-        In his spare time you can find him hiking, trying to set a Mario Kart record, or playing guitar.
+        In his spare time you can find him hiking, being too competitive at trivia, or playing guitar.
       </p>
     </div>
   );
