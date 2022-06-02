3b0fd057bafc003106bc8c8782a6d536628bb5d3
Matthew Carlson <carlson.matt.r@gmail.com>
2022-06-01 20:19:15 -0700

    more form changes

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,35 +2,56 @@ import React from "react";
 
 export default function Contact() {
   const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
     e.preventDefault();
-    if(!email){
-    alert("Please provide a valid email address")
+ 
+    const email = new RegExp(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    if (!email.test(e.target.email.value)) {
+      alert("Please provide a valid email address");
     } 
   };
-  
+
   return (
     <div>
       <h1 className="name">Contact Me</h1>
-      <form className="name">
-        <label for="name" tabIndex={1}>Name:</label>
+      <form className="name" onSubmit={handleFormSubmit}>
+        <label for="name" tabIndex={1}>
+          Name:
+        </label>
         <br></br>
         <input type="text" placeholder="Your name" tabIndex={2}></input>
-        <br></br><br></br>
-        <label for="email">Email:</label> 
         <br></br>
-        <input name="email" placeholder="example@email.com" tabIndex={3}></input>
-        <br></br><br></br>
+        <br></br>
+        <label for="email">Email:</label>
+        <br></br>
+        <input
+          name="email"
+          placeholder="example@email.com"
+          tabIndex={3}
+        ></input>
+        <br></br>
+        <br></br>
         <label for="message">Message: </label>
         <br></br>
-        <input name="message" placeholder="Your message here" tabIndex={4}></input>
-        <br></br><br></br>
-        <button type="button" onClick={handleFormSubmit} name="submit">Submit</button>
+        <input
+          name="message"
+          placeholder="Your message here"
+          tabIndex={4}
+        ></input>
+        <br></br>
+        <br></br>
+        <button type="button" name="submit">
+          Submit
+        </button>
       </form>
-      <br></br><br></br>
-        <br></br><br></br>
-        <h3 className="name">Telephone: (No Solicitors)</h3>
-        <h4 className="name">760 402 9439</h4>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <h3 className="name">Telephone: (No Solicitors)</h3>
+      <h4 className="name">760 402 9439</h4>
     </div>
   );
 }
